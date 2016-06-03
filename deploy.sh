eb37e108dbcdcd9b5c0156241ed6dd7e04ec02e5
Hoc Nguyen <hoc.nguyenviet90@gmail.com>
2016-06-03 16:18:09 +0700

    fix mongoURL

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-DEPLOY_HOSTNAME=galaxy.meteor.com MONGO_URL=mongodb://admin:password@ds023303.mlab.com:23303/meteormeetupmeteor meteor deploy meetup.designveloper.com
+DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy meetup.designveloper.com --settings settings.json
