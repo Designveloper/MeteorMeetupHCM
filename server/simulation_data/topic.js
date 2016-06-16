4ce160d5adff1453716098ae4e8861bc2f28829f
Hoc Nguyen <hoc.nguyenviet90@gmail.com>
2016-06-16 09:55:10 +0700

    add event_id to sm data

--- server/simulation_data/topic.js
+++ server/simulation_data/topic.js
@@ -6,7 +6,8 @@ Meteor.methods({
         image: '/img/home.png',
         title: 'Meteor for Android Application',
         timeEst: 30,
-        vote_id: '1'
+        vote_id: '1',
+        event_id: 1
       },
       {
         _id: '2',
@@ -14,7 +15,8 @@ Meteor.methods({
         title: 'Hello world to meteor',
         timeEst: 30,
         vote_id: '2',
-        content: 'Lorem ipsum dolor sit amet, vim ad fierent incorrupte disputationi, te eam debitis appellantur, ea malorum ceteros ius. Natum probatus definitionem id quo, utinam sententiae duo in. Et timeam detraxit vix, eu aeterno definitiones nam. Nec ex dolore reprimique.Vivendo fuisset definitiones vim ne, ad nobis dictas epicurei vis. Solum nostro scripta ex mel, quidam dolorum salutatus id pri, ex nostrud copiosae platonem mei. Sit mollis rationibus et, vix at quando signiferumque, at illum constituto mei. Ei eum aliquid facilisis, sit legere doming virtute ad. Prodesset sadipscing id eam.'
+        content: 'Lorem ipsum dolor sit amet, vim ad fierent incorrupte disputationi, te eam debitis appellantur, ea malorum ceteros ius. Natum probatus definitionem id quo, utinam sententiae duo in. Et timeam detraxit vix, eu aeterno definitiones nam. Nec ex dolore reprimique.Vivendo fuisset definitiones vim ne, ad nobis dictas epicurei vis. Solum nostro scripta ex mel, quidam dolorum salutatus id pri, ex nostrud copiosae platonem mei. Sit mollis rationibus et, vix at quando signiferumque, at illum constituto mei. Ei eum aliquid facilisis, sit legere doming virtute ad. Prodesset sadipscing id eam.',
+        event_id: 1
       }]
     for (let aData of data){
       if (!Topic.findOne(aData._id)){
