40b05a5db3ee6b7796ef7bd8ae20432f5282a49e
Hoc Nguyen <hoc.nguyenviet90@gmail.com>
2016-06-17 16:22:00 +0700

    fix group wiget

--- client/dashboard/group-list/group-list.js
+++ client/dashboard/group-list/group-list.js
@@ -3,6 +3,12 @@ Template.groupListTemplate.helpers({
     var user = Meteor.user();
     if (!user) return;
     var groupList = user.groups;
+    if (this.type == "all"){
+      return Group.find({}).fetch();
+    }
     return Group.find({_id: {$in: groupList}}).fetch();
+  },
+  "routeForGroup": function(){
+    return Meteor.absoluteUrl("group/"+this._id)
   }
 })
--- client/group/all-group/all-group-list/all-group-list.js
+++
@@ -1,5 +0,0 @@
-Template.allGroupListTemplate.helpers({
-  'groups': function(){
-    return Group.find({}).fetch();
-  }
-})
