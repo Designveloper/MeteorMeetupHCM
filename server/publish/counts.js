e3fb29a3f68dcc0e920ad58e646dab6ab94629fb
Hoc Nguyen <hoc.nguyenviet90@gmail.com>
2016-06-21 15:27:54 +0700

    summary detail

--- server/publish/counts.js
+++ server/publish/counts.js
@@ -12,3 +12,36 @@ Meteor.publish('countVote', function (type, reference_id) {
   }));
 })
 
+Meteor.publish('subCountEvent', function (type, data) {
+  console.log('dataSubCount' + type, data);
+  if (type === 'EventCountAge')
+    return Counts.publish(this, type + '-' + data.value, Vote.find({
+      type: 'event',
+      reference_id: data.eventId
+    },{ fields: { _id: 1, 'byUser': 1 }}
+    ),{countFromField: function(doc){
+      var user = Meteor.users.findOne(doc.byUser);
+      if (!user)
+        return 0;
+      if (user.profile.age === ""+data.value)
+        return 1;
+    }});
+  if (type === 'EventCountTitle')
+    return Counts.publish(this, type + '-' + data.value, Vote.find({
+      type: 'event',
+      reference_id: data.eventId
+    },{ fields: { _id: 1, 'byUser': 1 }}
+    ), {countFromField: function(doc){
+      var user = Meteor.users.findOne(doc.byUser);
+      if (!user)
+        return 0;
+      if (user.profile.title === data.value)
+        return 1;
+    }});
+  if (type === 'EventCountRate')
+    return Counts.publish(this, type + '-' + data.value, Vote.find({
+      type: 'event',
+      reference_id: data.eventId,
+      value: "" + data.value
+    }));
+});
