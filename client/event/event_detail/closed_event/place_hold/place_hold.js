39958652cd0f14df666774a3b2155a2365794e99
Hoc Nguyen <hoc.nguyenviet90@gmail.com>
2016-06-21 17:09:56 +0700

    change map for ended status event

--- client/event/event_detail/closed_event/place_hold/place_hold.js
+++ client/event/event_detail/closed_event/place_hold/place_hold.js
@@ -1,12 +1,6 @@
 Template.map2HoldTemplate.helpers({
-  'mapOptions': function(){
-    if (GoogleMaps.loaded()) {
-      // Map initialization options
-      return {
-        center: new google.maps.LatLng(-37.8136, 144.9631),
-        zoom: 8
-      };
-    }
+  'mapName': function () {
+    return "map-event-ended-" + this._id;
   }
 })
 Template.placeHoldTemplate.onCreated(function(){
