75edd3b56ca85e09727a29ec84aaea959df367fe
Hoc Nguyen <hoc.nguyenviet90@gmail.com>
2016-06-23 15:36:21 +0700

    realtime slide eventlist

--- client/dashboard/event-list/event-list.js
+++ client/dashboard/event-list/event-list.js
@@ -1,42 +1,25 @@
 Template.eventListTemplate.helpers({
-  'events': function () {
+  'renderData': function(){
+    var data = null;
+    var size = this.sizeSlide;
     var user = Meteor.user();
     if (!user) return;
     var groupList = user.groups;
     if (this.type == "details")
-      return EventData.find({
+      data = EventData.find({
         group_id: ENUM.groupId(),
         date: {$gte: new Date()},
       }).fetch();
-    return EventData.find({group_id: {$in: groupList}, date: {$gte: new Date()}}).fetch();
-  },
-  'subData': function(){
-    Meteor.subscribe('voteByTypeNId', 'event', this._id);
-  },
-  'routeToEvent': function () {
-    return Meteor.absoluteUrl("event/" + this._id);
-  },
-  'mapName': function () {
-    return "map-event-" + this._id;
-  },
-  'isJoinedEvent': function(){
-    var votes = Vote.find({type:'event', reference_id: this._id, byUser: Meteor.userId()}).fetch();
-    if (votes.length){
-      var vote = votes[0];
-      return vote.is_joined;
-    }
-  }
-});
+    else
+      data =EventData.find({group_id: {$in: groupList}, date: {$gte: new Date()}}).fetch();
 
-Template.eventListTemplate.events({
-  'change input': function(e,tpl){
-    var _id = e.currentTarget.getAttribute('data-id');
-    var data = ENUM.getDataByEvent(e);
-    var base = {type:'event', reference_id: _id, byUser: Meteor.userId()};
-    var vote = Vote.findOne(base);
-    if (vote)
-      return  Vote.update({_id: vote._id},{$set: data});
-    Vote.insert(_.extend(base,data));
+    setTimeout(function () {
+      var dom1 = document.getElementById("event-slide");
+      $(dom1).html('');
+      Blaze.renderWithData(Template.eventDataTemplate, {
+        data: data,
+        size: size
+      }, dom1);
+    }, 0);
   }
 })
-
