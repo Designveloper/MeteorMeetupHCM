175d7216d0ba564db57e24924d5b6d8ad5e50b78
Linh Vu <linhvn@dgroup.co>
2016-06-23 14:56:36 +0700

    datetimepicker

--- client/group/group-detail/calendar-upcoming/calendar-upcoming.js
+++ client/group/group-detail/calendar-upcoming/calendar-upcoming.js
@@ -1,1 +1,10 @@
-
+Template.calendarUpcomingTemplate.onRendered(function(){
+  $('#datetimepicker12').datepicker({
+    inline: true,
+    sideBySide: true,
+    multidate: true,
+    todayHighlight: true,
+    defaultViewDate: { year: 1977, month: 04, day: 25 },
+  });
+  $('#datetimepicker12').datepicker('setDate', '06/06/2016','06/16/2016','06/26/2016');
+})
