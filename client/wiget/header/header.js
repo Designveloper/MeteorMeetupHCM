67f45eec44cfe1b4306aa6c8e52fcea2a240ea28
linhdk <linhdk@dgroup.co>
2016-07-06 15:51:26 +0700

    Fix header for mobile page

--- client/wiget/header/header.js
+++ client/wiget/header/header.js
@@ -41,8 +41,14 @@ Template.header.helpers({
     var user = Meteor.user();
     if (!user) return "loading..";
     return ENUM.getAvatarCurrentUser(user) || ENUM.NO_AVATAR;
+  },
+  'showMenuMobile': function(){
+    var height = Session.get('window-height');
+    $('.navbar-collapse').css('height', height);
+    return Session.get('showMenuMobile');
   }
-})
+});
+
 Template.header.events({
   'click #signout-input': function(e){
     e.preventDefault();
@@ -53,5 +59,29 @@ Template.header.events({
   'click .navbar-nav li': function(e) {
     $('.navbar-nav li').removeClass('active');
     $(e.currentTarget).addClass('active');
+  },
+  'click .navbar-toggle': function(e) {
+    var value = Session.get('showMenuMobile');
+    Session.set('showMenuMobile', !value);
+    var height = $(window).height()- $('.navbar-header').height();
+    Session.set('window-height', height);
+
+    $('.navbar-collapse').css('left', '-4px');
+  },
+  'click .navbar-collapse li': function(e) {
+    Session.set('showMenuMobile', false);
+  },
+  'click .profile-btn': function(e) {
+    Session.set('showMenuMobile', false);
   }
-})
+});
+
+Template.header.onRendered(function(){
+  Session.set('showMenuMobile', false);
+  $('.navbar-collapse').css('left', '-500px');
+});
+
+Template.header.onCreated(function(){
+  Session.set('showMenuMobile', false);
+  $('.navbar-collapse').css('left', '-500px');
+});
